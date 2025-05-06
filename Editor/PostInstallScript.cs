268a9b98c6a3db073235f0a6c2c3edff2167bfab
charlie.song <charlie.song@bondee.com>
2025-05-06 15:26:22 +0800

    update: version to 96.6.4;

---
+++ Editor/PostInstallScript.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+
+namespace Editor
+{
+    public static class PostInstallScript
+    {
+        private const string PackageId = "Packages/com.mirrornetworking.mirror";
+
+        [InitializeOnLoadMethod]
+        public static void Execute()
+        {
+            var packageInfo = UnityEditor.PackageManager.PackageInfo.FindForAssetPath(PackageId);
+            if (packageInfo != null)
+            {
+                var packagePath = packageInfo.resolvedPath;
+                if (DeleteExcept(Path.Combine(packagePath, "Mirror"), Path.Combine(packagePath, "Mirror", "Assets")))
+                {
+                    AssetDatabase.Refresh();
+                }
+            }
+        }
+
+        private static bool DeleteExcept(string targetPath, string preservePath)
+        {
+            var isDelete = false;
+            if (!Directory.Exists(targetPath)) return false;
+            var metaPath = $"{preservePath}.meta";
+            if (!File.Exists(metaPath))
+            {
+                GenerateAMetaFile(metaPath);
+            }
+            foreach (var directory in Directory.GetDirectories(targetPath))
+            {
+                if (Path.GetFileName(directory) != Path.GetFileName(preservePath))
+                {
+                    isDelete = true;
+                    Directory.Delete(directory, true);
+                }
+            }
+
+            foreach (var file in Directory.GetFiles(targetPath))
+            {
+                if (Path.GetFileName(file) != Path.GetFileName(preservePath))
+                {
+                    isDelete = true;
+                    File.Delete(file);
+                }
+            }
+
+            return isDelete;
+        }
+
+        private static void GenerateAMetaFile(string metaFilePath)
+        {
+            var guid = Guid.NewGuid();
+            var formattedGuid = guid.ToString("N");
+            var contents = $@"
+fileFormatVersion: 1
+guid: {formattedGuid}
+PackageManifestImporter:
+externalObjects: {{}}
+userData: 
+assetBundleName: 
+assetBundleVariant:";
+            File.WriteAllText(metaFilePath, contents);
+        }
+    }
+}
